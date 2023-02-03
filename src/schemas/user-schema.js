ffecc07d4a8c3e81d1424cd3eb6af47a43c7a350
Micael9788 <97627646+Micael9788@users.noreply.github.com>
2023-02-03 16:25:33 -0300

    Create user-schema.js

---
+++ src/schemas/user-schema.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema(
+  {
+  user: String,
+  profile: {
+  name: { type: String, required: false },
+  img: { type: String, required: false },
+  verified: { type: String, required: false }
+  },
+  email: String,
+  password: String,
+  idade: String,
+  nasceu: String,
+  numero: String,
+  },
+  {
+		timestamps: true
+	}
+)
+
+module.exports = mongoose.model('users', schema)
